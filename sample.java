1a1e94a97d34be48724567002aba2d5169fe982c
takahiro4432 <t-sato@excite-software.co.jp>
2015-07-09 14:12:15 +0900

    Update sample.java

--- sample.java
+++ sample.java
@@ -2,10 +2,10 @@ import twitter4j.*;
 import twitter4j.auth.AccessToken;
 
 public class sample {
-	private static final String consumerKey       = "hOrVbBLFmeH4kliMigAv2QinG";
-	private static final String consumerSecret    = "dUYZq4WTqIlDVSDy5QpW0EXOXE3RRMLDeyShNMkrLGsMe5Vl5z";
-	private static final String accessToken       = "3269512034-I8URyMsTsiRq6ESyMx1tOGiKGLdLR7kSsWdLp8z";
-	private static final String accessTokenSecret = "pHuAOUZeHJxkX26GdF2TeJeBL7Qslb6Zg8fz8D35J6Kle";
+	private static final String consumerKey       = "";
+	private static final String consumerSecret    = "";
+	private static final String accessToken       = "";
+	private static final String accessTokenSecret = "";
 	
 	
 	private static final String tweet = "bot";
@@ -16,9 +16,9 @@ public class sample {
 		twitter.setOAuthAccessToken(new AccessToken(accessToken,accessTokenSecret));
 		try{
 			twitter.updateStatus(tweet);
-			System.out.println("ÉcÉCÅ[ÉgÇµÇΩÇÊÇó");
+			System.out.println("„ÉÑ„Ç§„Éº„Éà");
 		} catch(TwitterException e){
-			System.err.println("ÉcÉCÅ[Égé∏îs"+e.getMessage());
+			System.err.println("Â§±Êïó"+e.getMessage());
 		}
 		
 	}
